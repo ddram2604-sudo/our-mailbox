3d537d481e4d7409ddca4fd4b18f1f8f73910bfe
ddram2604-sudo <ddram.2604@gmail.com>
2026-06-27 14:51:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function openMailbox() {
-    alert("button works");
-    document.getElementById("welcome").style.display = "none";
-    document.getElementById("mailbox").style.display = "block";
-}
+alert("JS IS LOADED");
