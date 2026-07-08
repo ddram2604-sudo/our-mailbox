e52e6c82cdda7bc1ddd65858b816c4a9ecb6d6ac
ddram2604-sudo <ddram.2604@gmail.com>
2026-07-08 20:42:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const openButton = document.getElementById("openButton");
-
-openButton.addEventListener("click", () => {
-
-    document.body.classList.add("fade-out");
-
-    setTimeout(() => {
-        window.location.href = "mailbox.html";
-    }, 500);
-
-});
+alert("JavaScript is working!");
